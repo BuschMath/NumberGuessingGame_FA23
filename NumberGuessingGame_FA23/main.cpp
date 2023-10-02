f711d01e4b312e0dd22b3a6cd74e4957a43a057b
BuschMath <busch@iowacentral.edu>
2023-10-02 10:42:58 -0500

    Add project files.

---
+++ NumberGuessingGame_FA23/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to the Number Guessing Game!" << endl;
+	cout << "I'm thinking of a number between 1 and 10..." << endl;
+	cout << "Can you guess what it is?" << endl;
+
+	srand(time(NULL));
+	int number = rand() % 10 + 1;
+	int guess;
+	cin >> guess;
+
+	if (guess == number)
+	{
+		cout << "You guessed correctly!" << endl;
+	}
+	else
+	{
+		cout << "You guessed incorrectly! Try again." << endl;
+		
+		if (guess == number)
+		{
+			cout << "You guessed correctly!" << endl;
+		}
+		else
+		{
+			cout << "You guessed incorrectly! Try again." << endl;
+
+			if (guess == number)
+			{
+				cout << "You guessed correctly!" << endl;
+			}
+			else
+				cout << "You guessed incorrectly! The number was: " << number << endl;
+		}
+
+	}
+
+	return 0;
+}
