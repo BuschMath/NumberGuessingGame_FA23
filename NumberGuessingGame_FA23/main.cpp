85820dbe534c9362a9a9fd0b39eeb9794ca50c7a
BuschMath <busch@iowacentral.edu>
2023-10-16 10:53:11 -0500

    Loop update

--- NumberGuessingGame_FA23/main.cpp
+++ NumberGuessingGame_FA23/main.cpp
@@ -6,39 +6,58 @@ using namespace std;
 
 int main()
 {
-	cout << "Welcome to the Number Guessing Game!" << endl;
-	cout << "I'm thinking of a number between 1 and 10..." << endl;
-	cout << "Can you guess what it is?" << endl;
-
+	bool runAgain = true;
 	srand(time(NULL));
-	int number = rand() % 10 + 1;
-	int guess;
-	cin >> guess;
 
-	if (guess == number)
+	while(runAgain)
 	{
-		cout << "You guessed correctly!" << endl;
-	}
-	else
-	{
-		cout << "You guessed incorrectly! Try again." << endl;
+		cout << "Welcome to the Number Guessing Game!" << endl;
+		cout << "I'm thinking of a number between 1 and 10..." << endl;
+		cout << "How many guesses would you like?" << endl;
+
+		int guesses = 3;
+		cin >> guesses;
+
+		cout << "Can you guess what the number is?" << endl;
+
+		int number = rand() % 10 + 1;
+		int guess;
 		cin >> guess;
-		if (guess == number)
+
+		while (guesses > 0)
 		{
-			cout << "You guessed correctly!" << endl;
-		}
-		else
-		{
-			cout << "You guessed incorrectly! Try again." << endl;
-			cin >> guess;
 			if (guess == number)
 			{
 				cout << "You guessed correctly!" << endl;
+				break;
 			}
 			else
-				cout << "You guessed incorrectly! The number was: " << number << endl;
+			{
+				cout << "You guessed incorrectly!" << endl;
+				cout << "You have " << guesses - 1 << " guesses left." << endl;
+				cin >> guess;
+				guesses--;
+			}
 		}
 
+		if (guesses == 0)
+		{
+			cout << "You ran out of guesses!" << endl;
+			cout << "The number was " << number << "." << endl;
+		}
+
+		cout << "Would you like to play again? (y/n)" << endl;
+		char playAgain;
+		cin >> playAgain;
+
+		if (playAgain == 'y')
+		{
+			runAgain = true;
+		}
+		else
+		{
+			runAgain = false;
+		}
 	}
 
 	return 0;
